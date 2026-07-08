6d976aa2325677005ed3de76bc2aeb43a4ddd7f3
Patsch <p.euerle@shutdown-r.wtf>
2026-07-08 17:49:46 +0200

    fix: generate missing aliases.lmdb on startup (#6)

--- init.sh
+++ init.sh
@@ -6,4 +6,8 @@ echo "Generating Postfix regex maps for ${SENDER_ADDRESS}..."
 echo "/.+/    ${SENDER_ADDRESS}" > /etc/postfix/sender_canonical
 echo "/From:.*/ REPLACE From: ${SENDER_ADDRESS}" > /etc/postfix/header_checks
 
+# Fix missing aliases database error
+echo "Generating aliases database..."
+postalias lmdb:/etc/aliases
+
 echo "Postfix maps generated successfully."
